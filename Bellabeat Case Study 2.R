413c7e2beb3dd6bef3cf7d32909088e2cc1fde1e
gracedadzie <graciedadzie@gmail.com>
2023-02-08 23:15:56 -0500

    Comparing the number of IDs and the nuber of observations

--- Bellabeat Case Study 2.R
+++ Bellabeat Case Study 2.R
@@ -10,13 +10,36 @@ gitcreds_get() #create a github repository to track project changes
 #The Bellabeat app provides users with health data related to their activity, sleep, stress, menstrual cycle and mindfulness habits
 #This data helps users gain a better understanding of their health and make better choices to develop healthier habits
 #The Bellabeat app connects to their line of smart wellness products such as the Leaf (wellness bracelet, necklace, or clip), Time (wellness watch), Spring (water bottle)
-# Bellabeat also offers a subscription based membership program which gives users 24/7 access to personalized guidance on nutrition, activity, sleep, health and beauty, and mindfulness based on their personal goals.
+#Bellabeat also offers a subscription based membership program which gives users 24/7 access to personalized guidance on nutrition, activity, sleep, health and beauty, and mindfulness based on their personal goals.
 
 #Business Task:
 #1. Analyze a non-Bellabeat smart device: In this case we will use FitBit Fitness Tracker Data sourced from Kaggle user [Mobius](https://www.kaggle.com/datasets/arashnic/fitbit)
 
 install.packages('tidyverse')
-library(tidyverse) #instaling and loading common packages and libraries
+library(tidyverse) #installing and loading common packages and libraries
+daily_activity <-  read.csv("C:/Users/graci/Downloads/Fitabase Data 4.12.16-5.12.16/dailyActivity_merged.csv")
+head(daily_activity) #shows the first 6 rows of data in the data frame
+colnames(daily_activity) #provides the column names within the data frame
+str(daily_activity) #check for the structure of data to make sure it is appropriate for the column
+
+sleep_day <- read.csv("C:/Users/graci/Downloads/Fitabase Data 4.12.16-5.12.16/sleepDay_merged.csv")
+head(sleep_day)
+colnames(sleep_day)
+str(sleep_day)
+
+#We will merge the data using the ID field
+
+library(dplyr) #load dplyr package in order to use n_distinct
+n_distinct(daily_activity$Id)
+n_distinct(sleep_day$Id)
+
+#There's a discrepancy between number of unique values in daily activity (33) vs. sleep day data frames (24). Users may not wear watch to sleep.
+
+nrow(daily_activity)
+nrow(sleep_day)
+
+#
+
 
 #2. Select a Bellabeat product: Time (watch)
 #3. Analyze the smart device usage data to gain insights on how people are already using their smart devices
